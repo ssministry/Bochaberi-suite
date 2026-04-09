163bdb90c02b0a2d8a65373ea7f982947516a51b
ssministry <your-email@example.com>
2026-04-09 21:24:08 +0300

    Add simple test function hello.js

---
+++ netlify/functions/hello.js
@@ -0,0 +1,14 @@
+exports.handler = async (event, context) => {
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      message: 'Hello from Netlify Function!',
+      path: event.path,
+      method: event.httpMethod
+    })
+  };
+};
